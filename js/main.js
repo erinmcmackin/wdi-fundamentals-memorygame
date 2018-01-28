5b63f446d31dc099108f1be45845f0f41a1d0c27
Erin McMackin <erinmcmackin@Erins-MacBook-Pro.local>
2018-01-28 12:57:35 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,21 +1,7 @@
-var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = [];
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
 
-var cardOne = cards[0];
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardsInPlay[0]);
-console.log("User flipped " + cardsInPlay[1]);
-
-var isMatch = function () {
-	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
-		alert("Sorry, try again.");
-	}
-}
-
-isMatch();
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardThree);
